63a9db7b643819c87c2e10c029700c95010c601e
FirOX <54251660+FirOXjs@users.noreply.github.com>
2021-05-15 05:39:45 +0300

    Add files via upload

---
+++ beni-oku.md
@@ -0,0 +1,6 @@
+**Bu altyapı Mrk İmparatorluğu tarafından Matthe(Coders Area) için yapılmıştır ve Mrk İmparatorluğu izni ve Matthe izni olmadan hiçbir şekilde paylaşılamaz. Tüm telif hakları Mrk İmparatorluğu ve Matthe'ye aittir.
+İyi günler dileriz** <3
+
+[YOUTUBE](https://www.youtube.com/channel/UCZ0DgL77TQFNMwmnbvG8cuw)
+[Mrk İmparatorluğu](https://discord.gg/8Z2gwv7YHw)
+[Coders Area](https://discord.gg/2dJWBVgMYC)
---
+++ komutlar/dans-et.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+exports.run = async(client, message) => {
+      const vatan = new Discord.MessageEmbed()
+    .setAuthor('Mükemmel dans ediyorsun!')
+    .setColor(3447003)
+    .setImage(`https://i.gyazo.com/b1cbe83496e3bfe2dc2de30083f77dbe`)
+    return message.channel.send(vatan);
+} //mrk
+  //coders area
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['dans','danset'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'dans-et',
+  description: '',
+  usage: ''
+};
